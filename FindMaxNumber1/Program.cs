97d10d6947c2258283c5e1eaf010bdebd16d0648
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 12:51:07 +0530

    FindingMaximumNuber

---
+++ FindMaxNumber1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaxNumber1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            double output = Generics.MaximumFloatNumber(111.2, 22.0, 33.23);
+            Console.WriteLine(output);
+        }
+    }
+}
