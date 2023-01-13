a15aacbd2af249ff75e0b970541afcfb120ca94e
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 12:55:05 +0530

    FindingMaximumNumber

---
+++ FindMaxNumber2/Generics.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace FindMaxNumber2
+{
+    public class Generics
+    {
+        public static string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("fistValue, secondValue, thirdValue are same");
+        }
+    }
+}
---
+++ FindMaxNumber2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaxNumber2
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Numner");
+            string stringoutput = Generics.MaximumStringNumber("22", "33", "444");
+            Console.WriteLine(stringoutput);
+        }
+    }
+}
