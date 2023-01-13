835cb10bfb4443b4515e5d886efeb8bf4745c46c
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 11:45:53 +0530

    GenericsProgram

---
+++ PrintArray1/Generics.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace PrintArray1
+{
+    public class Generis
+    {
+        public static void toPrinrt(int[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+        public static void toPrinrt(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+        public static void toPrinrt(char[] inputArray)
+        {
+            foreach (char element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("............................");
+        }
+    }
+}
+
---
+++ PrintArray1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PrintArray1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArray = { 'S', 'A', 'N', 'G', 'M', 'E', 'S', 'H' };
+
+            Generis.toPrinrt(intArray);
+            Generis.toPrinrt(doubleArray);
+            Generis.toPrinrt(charArray);
+        }
+    }
+    
+}
