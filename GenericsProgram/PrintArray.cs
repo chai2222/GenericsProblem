89534ac223397da154caa8187ae859deb6c960df
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 11:11:18 +0530

    CheckTestCase

---
+++ GenericsProgram/PrintArray.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace GenericsProgram
+{
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+        public void toPrint()
+        {
+            //Console.WriteLine(this.inputArray);
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(".....................................");
+        }
+    }
+
+}
---
+++ GenericsProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericsProgram
+{
+    class Program
+    { 
+    public static void Main(string[] args)
+    {
+        int[] intArray = { 1, 2, 4, 5, 6, 7 };
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+        char[] charArray = { 'S', 'A', 'N', 'G', 'M', 'E', 'S', 'H' };
+
+        new PrintArray<int>(intArray).toPrint();
+        new PrintArray<double>(doubleArray).toPrint();
+        new PrintArray<char>(charArray).toPrint();
+    }
+}
+}
