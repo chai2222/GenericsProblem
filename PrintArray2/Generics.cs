e06a16b23c37abbdc9e459f3e57ca3f3e1bcf8e9
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 12:43:19 +0530

    GenericsProgram

---
+++ PrintArray2/Generics.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace PrintArray2
+{
+    public class Generics
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(".................................");
+        }
+    }
+}
---
+++ PrintArray2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PrintArray2
+{
+    public class Priogarm
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, };
+            double[] doubleArray = { 1.2, 2.1, 3.2, 4.4, 5.5, 6.3 };
+            char[] charArray = { 'S', 'A', 'N', 'G', 'M', 'E', 'S', 'H' };
+
+            Generics.toPrint<int>(intArray);
+            Generics.toPrint<double>(doubleArray);
+            Generics.toPrint<char>(charArray);
+        }
+    }
+}
