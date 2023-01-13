7ac10acfa4a594aa44f1e174ca7ca4b008fc0809
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 11:33:22 +0530

    RefactorProgram

---
+++ RefactorPro1/GenericsMaximum.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace RefactorPro1
+{
+    public class GemericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public GemericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+               secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return thirdValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) > 0 ||
+               thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+        public T MaxMethod()
+        {
+            T max = GemericMaximum<T>.MaxValue(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+    }
+    }
---
+++ RefactorPro1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace RefactorPro1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            //int[] intArray = { 112, 344, 432, 555, 678 };
+            GemericMaximum<int> generic = new GemericMaximum<int>(2, 3, 1);
+            int result = generic.MaxMethod();
+            Console.WriteLine(result);
+
+
+            //double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GemericMaximum<double> generic1 = new GemericMaximum<double>(21.2, 42.2, 53.2);
+            double result1 = generic1.MaxMethod();
+            Console.WriteLine(result1);
+        }
+    }
+}
