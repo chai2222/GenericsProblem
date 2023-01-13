3a701fd7e808ae1bc961a2ba015385bb44a574b2
chai2222 <chaitanayajagadale6@gmail.com>
2023-01-13 11:28:02 +0530

    FindMax

---
+++ TestCase1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestCase1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Number");
+            int output = Generics.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
